d1f97559ab6e71a2d85738a020c7fea9f880303e
izumiano <izumiano@users.noreply.github.com>
2026-05-18 11:03:51 +0200

    make it work

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,37 @@
-import { logVerbose } from "@izumiano/vite-logger";
+const replacements: { [key: string]: string } = {};
 
-logVerbose("Hello World");
+const pattern = new RegExp(
+	"\\b(" +
+		Object.keys(replacements)
+			.map((key) => key.replaceAll(/[.*+?^${}()|[\]\\]/g, "\\$&"))
+			.join("|") +
+		")\\b",
+	"gi",
+);
+
+function doTextReplace(text: string) {
+	const newText = replacements[text.toLowerCase()];
+	if (text.toUpperCase() === text) {
+		return newText.toUpperCase();
+	}
+	if (text[0].toUpperCase() === text[0]) {
+		return newText[0].toUpperCase() + newText.slice(1);
+	}
+	return newText;
+}
+
+function recursiveReplace(elem: ChildNode) {
+	if (elem.nodeName === "FORM") {
+		return;
+	}
+
+	if (elem.nodeType === Node.TEXT_NODE) {
+		elem.nodeValue = elem.nodeValue?.replaceAll(pattern, doTextReplace) ?? null;
+	}
+
+	for (const child of elem.childNodes) {
+		recursiveReplace(child);
+	}
+}
+
+recursiveReplace(document.body);
