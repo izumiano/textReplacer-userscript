6cbaa958f91ebfa1b2d9ca6ce57d49a5a019dee9
izumiano <izumiano@users.noreply.github.com>
2026-05-19 10:33:31 +0200

    dont do anythingif no replacements

--- textreplacer/src/main.ts
+++ textreplacer/src/main.ts
@@ -4,38 +4,41 @@ const replacements: { [key: string]: string } = JSON.parse(
 	localStorage.getItem(localStorageMapKey) ?? "{}",
 );
 
-const pattern = new RegExp(
-	"\\b(" +
-		Object.keys(replacements)
-			.map((key) => key.replaceAll(/[.*+?^${}()|[\]\\]/g, "\\$&"))
-			.join("|") +
-		")\\b",
-	"gi",
-);
+if (Object.entries(replacements).length > 0) {
+	const pattern = new RegExp(
+		"\\b(" +
+			Object.keys(replacements)
+				.map((key) => key.replaceAll(/[.*+?^${}()|[\]\\]/g, "\\$&"))
+				.join("|") +
+			")\\b",
+		"gi",
+	);
 
-function doTextReplace(text: string) {
-	const newText = replacements[text.toLowerCase()];
-	if (text.toUpperCase() === text) {
-		return newText.toUpperCase();
+	function doTextReplace(text: string) {
+		const newText = replacements[text.toLowerCase()];
+		if (text.toUpperCase() === text) {
+			return newText.toUpperCase();
+		}
+		if (text[0].toUpperCase() === text[0]) {
+			return newText[0].toUpperCase() + newText.slice(1);
+		}
+		return newText;
 	}
-	if (text[0].toUpperCase() === text[0]) {
-		return newText[0].toUpperCase() + newText.slice(1);
+
+	function recursiveReplace(elem: ChildNode) {
+		if (elem.nodeName === "FORM") {
+			return;
+		}
+
+		if (elem.nodeType === Node.TEXT_NODE) {
+			elem.nodeValue =
+				elem.nodeValue?.replaceAll(pattern, doTextReplace) ?? null;
+		}
+
+		for (const child of elem.childNodes) {
+			recursiveReplace(child);
+		}
 	}
-	return newText;
+
+	recursiveReplace(document.body);
 }
-
-function recursiveReplace(elem: ChildNode) {
-	if (elem.nodeName === "FORM") {
-		return;
-	}
-
-	if (elem.nodeType === Node.TEXT_NODE) {
-		elem.nodeValue = elem.nodeValue?.replaceAll(pattern, doTextReplace) ?? null;
-	}
-
-	for (const child of elem.childNodes) {
-		recursiveReplace(child);
-	}
-}
-
-recursiveReplace(document.body);
